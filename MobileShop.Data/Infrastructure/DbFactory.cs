267fb0c121e6877c1b6ac50585f1393c5ce5e79f
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-06-23 23:43:33 +0700

    lesson6_DesginClassForEFCodeFirst_Time1

---
+++ MobileShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileShop.Data.Infrastructure
+{
+    public class DbFactory: Disposable,IDbFactory
+    {
+        private MobileShopDbContext dbContext;
+
+        public MobileShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new MobileShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
