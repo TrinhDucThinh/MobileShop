2a1fa3ccacb6b4d7c6f2fdaff7f159e6e47f71b0
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-06-26 05:50:47 +0700

    - create module and controller, display message from controller to view by $scope

---
+++ MobileShop.Web/Scripts/plugins/spa/app.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../angular/angular.js" />
+var myModule = angular.module("myModule", []);
+
+//register controller
+myModule.controller("myController", myController);
+
+myController.$inject("$scope");
+
+//declare controller
+
+function myController($scope) {
+    $scope.message = "This is my message from controller";
+}
