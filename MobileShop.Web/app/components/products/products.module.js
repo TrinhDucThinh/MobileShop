bfd7e45446aa538b0e94f4cf8b51b6168fc4985c
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-07-03 07:43:10 +0700

    update-routing

--- MobileShop.Web/app/components/products/products.module.js
+++ MobileShop.Web/app/components/products/products.module.js
@@ -1,19 +1,19 @@
 ﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
 
 (function () {
-	angular.module('mobileshop.products', ['mobileshop.common']).config(config);
+    angular.module('mobileshop.products', ['mobileshop.common']).config(config);
 
-	config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-	function config($stateProvider,$urlRouterProvider) {
-		$stateProvider.state('products', {
-			url: "/products",
-			tempateUrl: "/app/components/products/productListView.html",
-			controller:"productListController"
-		}).state('product_add', {
-			url: "/product_add",
-			tempateUrl: "/app/components/products/productAddView.html",
-			controller: "productAddController"
-		});
-	};
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: "/products",
+            templateUrl: "/app/components/products/productListView.html",
+            controller: "productListController"
+        }).state('product_add', {
+            url: "/product_add",
+            templateUrl: "/app/components/products/productAddView.html",
+            controller: "productAddController"
+        });
+    }
 })();
