b6a5d4f2a8a8b81c96d2b7f2f3b5097caff48f7c
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-07-04 15:11:46 +0700

    pagging for list product category

--- MobileShop.Web/Api/ProductCategoryController.cs
+++ MobileShop.Web/Api/ProductCategoryController.cs
@@ -3,7 +3,9 @@ using MobileShop.Model.Models;
 using MobileShop.Service;
 using MobileShop.Web.Infrastructure.Core;
 using MobileShop.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -21,13 +23,26 @@ namespace MobileShop.Web.Api
         }
 
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
+                int totalRow = 0;
                 var model = _productCategoryService.GetAll();
-                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
+                totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return response;
             });
         }
---
+++ MobileShop.Web/Infrastructure/Core/PaginationSet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MobileShop.Web.Infrastructure.Core
+{
+    public class PaginationSet<T>
+    {
+        public int Page { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return (Items != null) ? Items.Count() : 0;
+            }
+        }
+
+        public int TotalCount { get; set; }
+
+        public IEnumerable<T> Items { get; set; }
+    }
+}
