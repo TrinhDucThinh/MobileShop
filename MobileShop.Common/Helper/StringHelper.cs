c78a8a61fd1eba2f84902f650cf431eb3d113961
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-07-04 16:00:01 +0700

    add string helper

---
+++ MobileShop.Common/Helper/StringHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileShop.Common.Helper
+{
+    public static class StringHelper
+    {
+        //public static string ConvertString(string stringInput)
+        //{
+        //    stringInput = stringInput.ToUpper();
+        //    string convert = @"ĂÂÀẰẦÁẮẤẢẲẨÃẴẪẠẶẬỄẼỂẺÉÊÈỀẾẸỆÔÒỒƠỜÓỐỚỎỔỞÕỖỠỌỘỢƯÚÙỨỪỦỬŨỮỤỰÌÍỈĨỊỲÝỶỸỴĐăâàằầáắấảẳẩãẵẫạặậễẽểẻéêèềếẹệôòồơờóốớỏổởõỗỡọộợưúùứừủửũữụựìíỉĩịỳýỷỹỵđ";
+        //    string To = "AAAAAAAAAAAAAAAAAEEEEEEEEEEEOOOOOOOOOOOOOOOOOUUUUUUUUUUUIIIIIYYYYYDaaaaaaaaaaaaaaaaaeeeeeeeeeeeooooooooooooooooouuuuuuuuuuuiiiiiyyyyyd";
+        //    for (int i = 0; i & lt; To.Length; i++)
+        //    {
+        //          stringInput = stringInput.Replace(convert[i], To[i]);
+        //    }
+        //    return stringInput;
+                  
+        //}
+    }
+}
