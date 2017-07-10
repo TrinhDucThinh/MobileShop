9e478f87afdd32654e009218d50c6356578ab8bb
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-07-10 23:31:03 +0700

    add the form login for admin

--- MobileShop.Web/app/components/products/products.module.js
+++ MobileShop.Web/app/components/products/products.module.js
@@ -6,16 +6,20 @@
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
     function config($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('products', {
+        $stateProvider
+            .state('products', {
             url: "/products",
+            parent: 'base',
             templateUrl: "/app/components/products/productListView.html",
             controller: "productListController"
         }).state('product_add', {
             url: "/product_add",
+            parent: 'base',
             templateUrl: "/app/components/products/productAddView.html",
             controller: "productAddController"
         }).state('product_edit', {
             url: "/product_edit/:id",
+            parent: 'base',
             templateUrl: "/app/components/products/productEditView.html",
             controller: "productEditController"
         });
