bf9e889ecf071483b660beae6639dc4decf2029a
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-07-11 22:29:13 +0700

    Login admin with Asp Identity

---
+++ MobileShop.Web/Api/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using MobileShop.Service;
+using MobileShop.Web.Infrastructure.Core;
+using System.Web.Mvc;
+
+namespace MobileShop.Web.Api
+{
+    [RoutePrefix("api/home")]
+    [Authorize]
+    public class HomeController : ApiControllerBase
+    {
+        private IErrorService _errorService;
+
+        public HomeController(IErrorService errorService) : base(errorService)
+        {
+            this._errorService = errorService;
+        }
+
+        [HttpGet]
+        [Route("TestMethod")]
+        public string TestMethod()
+        {
+            return "Hello, TEDU Member. ";
+        }
+    }
+}
--- MobileShop.Web/app/app.js
+++ MobileShop.Web/app/app.js
@@ -2,7 +2,9 @@
     angular.module('mobileshop',
         ['mobileshop.products',
         'mobileshop.product_categories',
-        'mobileshop.common']).config(config);
+        'mobileshop.common'])
+        .config(config)
+        .config(configAuthentication);;
 
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
@@ -24,5 +26,34 @@
                 controller: "homeController"
             });
         $urlRouterProvider.otherwise('/login');
+    }
+
+    function configAuthentication($httpProvider) {
+        $httpProvider.interceptors.push(function ($q, $location) {
+            return {
+                request: function (config) {
+
+                    return config;
+                },
+                requestError: function (rejection) {
+
+                    return $q.reject(rejection);
+                },
+                response: function (response) {
+                    if (response.status == "401") {
+                        $location.path('/login');
+                    }
+                    //the same response/modified/or a new one need to be returned.
+                    return response;
+                },
+                responseError: function (rejection) {
+
+                    if (rejection.status == "401") {
+                        $location.path('/login');
+                    }
+                    return $q.reject(rejection);
+                }
+            };
+        });
     }
 })();
