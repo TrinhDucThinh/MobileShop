3af59369128d42164eeda9f4ac810f4f31cb05c6
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-07-14 22:09:03 +0700

    add client layout resource

--- MobileShop.Web/Controllers/HomeController.cs
+++ MobileShop.Web/Controllers/HomeController.cs
@@ -26,5 +26,23 @@ namespace MobileShop.Web.Controllers
 
             return View();
         }
+
+        [ChildActionOnly]
+        public ActionResult Footer()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Header()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Category()
+        {
+            return PartialView();
+        }
     }
 }
